4b0b319281016ef68f0ff500c0014e34664b89a1
guimaodai <wyyxdgm@163.com>
2016-10-17 15:20:37 +0800

    add .git

--- bin/init.sh
+++ bin/init.sh
@@ -2,7 +2,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR/..
 ROOTPATH=`pwd`
-if [ ! -d ocr ]; then
+if [ ! -d ocr/.git ]; then
 	git clone https://github.com/mateogianolio/ocr.git
 fi
 if [ ! -d ocr/node_modules ]; then
